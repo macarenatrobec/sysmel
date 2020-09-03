837db490ec09acb48f5eee437ffdf1a5bd9175f6
Macarena <Macarena@MacBook-Pro-de-Macarena.local>
2020-09-02 23:55:39 -0300

    first commit

---
+++ anim.js
@@ -0,0 +1,25 @@
+
+var someElement = document.querySelector('#inView');
+
+window.onscroll = function(){
+    if(someElement.getBoundingClientRect().bottom <= 0){
+        function counter(id, start, end, duration) {
+            let obj = document.getElementById(id),
+             current = start,
+             range = end - start,
+             increment = end > start ? 1 : -1,
+             step = Math.abs(Math.floor(duration / range)),
+             timer = setInterval(() => {
+              current += increment;
+              obj.textContent = current;
+              if (current == end) {
+               clearInterval(timer);
+              }
+             }, step);
+           }
+           counter("count1", 0, 13, 1000);
+           counter("count2", 0, 295, 1000);
+           counter("count3", 0, 106, 1000);
+           counter("count4", 0, 35, 1000);
+    }
+}
